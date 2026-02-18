87d5bfafe3adc2775c9940caa4b4ab18562f1cb3
Seif Essam <seifessam60@gmail.com>
2026-02-18 15:11:44 +0200

    feat: Initialize full-stack application with Clerk authentication, API services, and React Query integration.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(clerkMiddleware());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: ENV.FRONTEND_URL }));
+app.use(cors({ origin: ENV.FRONTEND_URL, credentials: true }));
 
 // API Routes
 app.use("/api", routes);
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -11,11 +12,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
 }
 
+// Create a client
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>,
